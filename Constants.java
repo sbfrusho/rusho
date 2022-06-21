2f907db1dca73b08c0e83e054264d6358a6d3a65
sbfrusho <102538412+sbfrusho@users.noreply.github.com>
2022-06-21 23:40:30 +0600

    Add files via upload
    done

---
+++ Constants.java
@@ -0,0 +1,17 @@
+public class Constants {
+    public String loadData = "Loading data ...";
+    public String dataLoad = "Data Loaded.";
+    public String dateTime = "dd/mm/yyyy-hh:mm:ss a";
+    public String listUpdate = "\nList last updated on ";
+    public String dataFound = "We found it!";
+    public String wordFound = " word(s) found";
+    public String fileName = "students.txt";
+    public String numAdd = "+";
+    public String showList = "a";
+    public String randomName = "r";
+    public String findName = "?";
+    public String countWord = "c";
+    public String split = ", ";
+    public char comma = ',';
+}
+
