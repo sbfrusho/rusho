7f8706a80a7f179cfff8822333e1e414099cf267
sbfrusho <rushocseru28@gmail.com>
2022-08-16 13:10:27 +0600

    final improve done

--- Constants.java
+++ Constants.java
@@ -1,4 +1,5 @@
 public class Constants {
+//  All string laterals literals are replaced with constants
     public String loadData = "Loading data ...";
     public String dataLoad = "Data Loaded.";
     public String dateTime = "dd/mm/yyyy-hh:mm:ss a";
@@ -6,12 +7,13 @@ public class Constants {
     public String dataFound = "We found it!";
     public String wordFound = " word(s) found";
     public String fileName = "students.txt";
-    public String numAdd = "+";
+    public String addName = "+";
     public String showList = "a";
     public String randomName = "r";
     public String findName = "?";
     public String countWord = "c";
     public String split = ", ";
     public char comma = ',';
+    public String wrongArgument = "Wrong argument passed.Try again.";
 }
 
--- StudentList.java
+++ StudentList.java
@@ -8,6 +8,7 @@ public class StudentList {
 
 	public static Constants constants = new Constants();
 	public static String studentName;
+//		Write names to the file
 	public static void writeFile(String readName){
 		try {
 			BufferedWriter bufferedWriter = new BufferedWriter(
@@ -17,27 +18,31 @@ public class StudentList {
 			bufferedWriter.close();
 		} catch (Exception e) {}
 	}
+
+//		Read names from the file
 	public static void readFile(){
 		try {
 			BufferedReader bufferedReader = new BufferedReader
 					(new InputStreamReader
 							(new FileInputStream(constants.fileName)));
 			studentName = bufferedReader.readLine();
-			//bufferedReader.close();
 		} catch(Exception e){}
 	}
 	public static void main(String[] args) {
 
 //		Check arguments
+//		Shows the list of names
 		if(args[0].equals(constants.showList)) {
 			System.out.println(constants.loadData);
 			readFile();
 			String names[] = studentName.split(constants.split);
-			for(String n : names) {
-				System.out.println(n);
+			for (String name : names) {
+				System.out.println(name);
 			}
 			System.out.println(constants.dataLoad);
 		}
+
+//		Displays a random name
 		else if(args[0].equals(constants.randomName)) {
 			System.out.println(constants.loadData);
 			readFile();
@@ -45,18 +50,22 @@ public class StudentList {
 			System.out.println(names[new Random().nextInt(names.length)]);
 			System.out.println(constants.dataLoad);
 		}
-		else if(args[0].contains(constants.numAdd)){
+
+//		Add a name to the file
+		else if(args[0].contains(constants.addName)){
 			System.out.println(constants.loadData);
 			readFile();
 			writeFile(studentName + constants.split + args[0].substring(1) + constants.listUpdate + new SimpleDateFormat(constants.dateTime).format(new Date()));
 			System.out.println(constants.dataLoad);
 		}
+
+//		Checks whether a name exists or not in the file
 		else if(args[0].contains(constants.findName)) {
 			System.out.println(constants.loadData);
 			readFile();
 			String names[] = studentName.split(constants.split);
 			int idx;
-			for( idx = 0; idx<names.length; idx++) {
+			for(idx = 0; idx<names.length; idx++) {
 				if(names[idx].equals(args[0].substring(1))) {
 					System.out.println(args[0].substring(1) +" found at index " +idx);
 					break;
@@ -65,8 +74,9 @@ public class StudentList {
 			if(idx == names.length){
 				System.out.println(args[0].substring(1) +" does not found");
 			}
-
 		}
+
+//		Counts how many words are in the file
 		else if(args[0].contains(constants.countWord)) {
 			System.out.println(constants.loadData);
 			readFile();
@@ -78,6 +88,11 @@ public class StudentList {
 				}
 			}
 			System.out.println((count+1) + constants.wordFound);
+		}
+
+//		To handle wrong passed argument
+		else {
+			System.out.println(constants.wrongArgument);
 		}
 	}
 }
